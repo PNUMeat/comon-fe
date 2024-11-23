03c4ceb4f9f73d6ce5a62fc5d05224b859604559
Jisu Lee <dlwltn0430@naver.com>
2024-11-23 23:14:43 +0900

    design: reset.css 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import '@/styles/reset.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import App from './App.tsx';
