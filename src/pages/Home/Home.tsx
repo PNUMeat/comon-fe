fa120e0418efd243654e8a50ffed4ac2c93fd193
JaeAnHan <jaean1999@naver.com>
2024-11-24 17:43:13 +0900

    feat: 공통 컴포넌트 사용법 및 레이아웃 확인 용도로 추가

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
 import { CommonLayout } from '@/components/commons/CommonLayout';
+import { PageSectionHeader } from '@/components/commons/PageSectionHeader';
 
 export const Home = () => {
   return (
     <CommonLayout>
       <div>코드몬스터 랜딩페이지</div>
+      <PageSectionHeader h={40}>타이틀</PageSectionHeader>
     </CommonLayout>
   );
 };
