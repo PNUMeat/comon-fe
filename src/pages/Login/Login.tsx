24b31bfc691b63517efe1586f138ddcd05811171
JaeAnHan <jaean1999@naver.com>
2024-11-24 19:31:30 +0900

    feat: 로그인 페이지 레이아웃 완성

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,3 +1,4 @@
+import { BackgroundGradient } from '@/components/commons/BackgroundGradient';
 import { CommonLayout } from '@/components/commons/CommonLayout';
 import { Container } from '@/components/commons/Container';
 import { GlassCard } from '@/components/commons/GlassCard';
@@ -13,6 +14,7 @@ export const Login = () => {
     <CommonLayout>
       <Container maxW={1090}>
         <PageSectionHeader h={h}>로그인</PageSectionHeader>
+        <BackgroundGradient />
         <Spacer h={80} />
         <GlassCard>
           <LoginForm h={324} />
