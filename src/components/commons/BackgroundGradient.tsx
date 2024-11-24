7c300b87aa27f65d67225d2a0c5fc389bfcd200e
JaeAnHan <jaean1999@naver.com>
2024-11-24 20:02:01 +0900

    chore: BackgroundGradient 주석 제거

--- src/components/commons/BackgroundGradient.tsx
+++ src/components/commons/BackgroundGradient.tsx
@@ -2,19 +2,13 @@ import styled from '@emotion/styled';
 
 export const BackgroundGradient = styled.div`
   width: 940px;
-  // width: 491px;
   height: 491px;
-  // height: 940px;
-  // border-radius: 940px 940px 0 0;
   border-radius: 0 0 600px 600px;
-  // border-radius: 50%;
   background: radial-gradient(circle, #c2c5fb 20%, #fda2d0 70%);
-  // background: radial-gradient(circle, #fda2d0 10%, #c2c5fb 50%);
   position: absolute;
   filter: blur(70px);
   opacity: 0.3;
   transform-origin: center;
   transform: rotate(180deg);
-  // transform: rotate(90deg);
   z-index: -1;
 `;
