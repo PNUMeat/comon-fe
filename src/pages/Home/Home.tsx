43664180b88ffd91bd9362a4b0837c2237941c07
JaeAnHan <jaean1999@naver.com>
2024-11-24 17:44:07 +0900

    feat: 공통 컴포넌트 사용법 확인하는 페이지 수정

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,8 +4,7 @@ import { PageSectionHeader } from '@/components/commons/PageSectionHeader';
 export const Home = () => {
   return (
     <CommonLayout>
-      <div>코드몬스터 랜딩페이지</div>
-      <PageSectionHeader h={40}>타이틀</PageSectionHeader>
+      <PageSectionHeader h={40}>코드몬스터 랜딩페이지</PageSectionHeader>
     </CommonLayout>
   );
 };
