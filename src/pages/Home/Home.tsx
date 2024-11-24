dc93d9b1c69d090bfa2fda515dec3e784d919d86
JaeAnHan <jaean1999@naver.com>
2024-11-24 17:07:57 +0900

    feat: Home 컴포넌트 추가 및 라우팅 적용
    - 랜딩 페이지 작업전 기본적인 작업을 추가했습니다

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+import { CommonLayout } from '@/components/commons/CommonLayout';
+
+export const Home = () => {
+  return (
+    <CommonLayout>
+      <div>코드몬스터 랜딩페이지</div>
+    </CommonLayout>
+  );
+};
