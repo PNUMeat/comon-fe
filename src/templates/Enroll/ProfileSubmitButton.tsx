3d4c612a54a7b3007cddd2fed4359ac2c7830f1c
JaeAnHan <jaean1999@naver.com>
2024-11-26 01:47:10 +0900

    feat: EnrollTemplate의 ProfileSubmitButton 컴포넌트 추가

---
+++ src/templates/Enroll/ProfileSubmitButton.tsx
@@ -0,0 +1,50 @@
+import styled from '@emotion/styled';
+
+const SubmitButton = styled.button<{ disabled: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  width: 675px;
+  height: 57px;
+  padding: 28px 72px 26px 72px;
+  border-radius: 20px;
+  font-family: Pretendard, sans-serif;
+  font-size: 18px;
+  font-weight: 600;
+  line-height: normal;
+  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
+
+  ${({ disabled }) =>
+    !disabled &&
+    `
+    background: #ffffff;
+    border: 3px solid #cdcfff;
+    box-shadow: 5px 7px 11.6px 0px rgba(63, 63, 77, 0.07);
+    color: #333333;
+
+    &:hover {
+      background: #f5f5ff;
+    }
+  `}
+
+  ${({ disabled }) =>
+    disabled &&
+    `
+    background: #f2f2f2;
+    box-shadow: 3px 6px 8.3px 0px rgba(63, 63, 77, 0.07) inset;
+    color: #cccccc;
+    border: none;
+  `}
+  
+  span {
+    font-size: 16px;
+`;
+
+export const ProfileSubmitButton = ({ disabled }: { disabled: boolean }) => {
+  return (
+    <SubmitButton disabled={disabled}>
+      <span>🚀 코몬 시작하기!</span>
+    </SubmitButton>
+  );
+};
