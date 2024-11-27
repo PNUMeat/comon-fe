be927011ddb5d2b61df7a85debf1d677d715a215
Jisu Lee <dlwltn0430@naver.com>
2024-11-27 21:20:43 +0900

    refactor: BackgroundGradient에 count, positions props 추가

--- src/components/commons/BackgroundGradient.tsx
+++ src/components/commons/BackgroundGradient.tsx
@@ -1,14 +1,54 @@
 import styled from '@emotion/styled';
 
-export const BackgroundGradient = styled.div`
+import { Flex } from './Flex';
+
+interface GradientBackgroundProps {
+  count: number;
+  positions: { top?: string; left?: string; right?: string; bottom?: string }[];
+}
+
+export const BackgroundGradient: React.FC<GradientBackgroundProps> = ({
+  count,
+  positions,
+}) => {
+  return (
+    <BackgroundContainer>
+      <Flex justify="center">
+        {Array.from({ length: count }).map((_, index) => (
+          <GradientCircle
+            key={index}
+            top={positions[index]?.top}
+            left={positions[index]?.left}
+            right={positions[index]?.right}
+            bottom={positions[index]?.bottom}
+          />
+        ))}
+      </Flex>
+    </BackgroundContainer>
+  );
+};
+
+const BackgroundContainer = styled.div`
+  position: relative;
+`;
+
+const GradientCircle = styled.div<{
+  top?: string;
+  left?: string;
+  right?: string;
+  bottom?: string;
+}>`
+  position: absolute;
+  top: ${(props) => props.top || 'auto'};
+  left: ${(props) => props.left || 'auto'};
+  right: ${(props) => props.right || 'auto'};
+  bottom: ${(props) => props.bottom || 'auto'};
   width: 940px;
   height: 491px;
-  border-radius: 0 0 600px 600px;
-  background: radial-gradient(circle, #c2c5fb 20%, #fda2d0 70%);
-  position: absolute;
-  filter: blur(70px);
+  border-radius: 600px 600px 0 0;
+  background: radial-gradient(100% 100% at 50% 100%, #c2c5fb 40%, #fda2d0 100%);
+  filter: blur(50px);
   opacity: 0.3;
   transform-origin: center;
-  transform: rotate(180deg);
   z-index: -1;
 `;
