e563bbd2682dd3509297036d7d29649bed554a43
JaeAnHan <jaean1999@naver.com>
2024-11-28 20:40:26 +0900

    feat: TeamSubjectRadio 추가

---
+++ src/templates/Team/TeamSubjectRadio.tsx
@@ -0,0 +1,40 @@
+import { Flex } from '@/components/commons/Flex';
+
+import { teamSubjectAtom } from '@/store/form';
+import styled from '@emotion/styled';
+import { useAtom } from 'jotai';
+
+const TeamSubjectButton = styled.button<{ isSelected: boolean }>`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  height: 28px;
+  padding: 2px 20px;
+  border: 1px solid ${({ isSelected }) => (isSelected ? '#cdcfff' : '#a3bffa')};
+  border-radius: 34px;
+  background: ${({ isSelected }) => (isSelected ? '#535353' : '#fff')};
+  font-size: 16px;
+  color: ${({ isSelected }) => (isSelected ? '#fff' : '#333333')};
+  cursor: pointer;
+  transition: all 0.2s;
+`;
+
+const options = ['study', 'code'];
+
+export const TeamSubjectRadio = () => {
+  const [subject, setSubject] = useAtom(teamSubjectAtom);
+
+  return (
+    <Flex gap={'6px'}>
+      {options.map((option) => (
+        <TeamSubjectButton
+          isSelected={subject === option}
+          key={option}
+          onClick={() => setSubject(option)}
+        >
+          {option}
+        </TeamSubjectButton>
+      ))}
+    </Flex>
+  );
+};
