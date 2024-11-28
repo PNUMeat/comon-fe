b963c8f71d0a18eb09c7ff47eda96f8f734f3c42
JaeAnHan <jaean1999@naver.com>
2024-11-28 20:51:46 +0900

    feat: TeamSubmitButton 추가

---
+++ src/templates/Team/TeamSubmitButton.tsx
@@ -0,0 +1,14 @@
+import { ComonFormSubmitButton } from '@/components/commons/Form/ComonFormSubmitButton';
+
+import { isTeamRegistrationValidAtom } from '@/store/form';
+import { useAtomValue } from 'jotai';
+
+export const TeamSubmitButton = () => {
+  const isGoodToGo = useAtomValue(isTeamRegistrationValidAtom);
+
+  return (
+    <ComonFormSubmitButton disabled={!isGoodToGo}>
+      <span>🚀 팀 생성하기</span>
+    </ComonFormSubmitButton>
+  );
+};
