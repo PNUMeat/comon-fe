574dba91c77bbfcb8d19c550a6d08a90a50a0b7e
JaeAnHan <jaean1999@naver.com>
2024-11-29 13:27:17 +0900

    feat: url과 매핑되는 FORM_TITLE 레코드 추가

--- src/routes/path.tsx
+++ src/routes/path.tsx
@@ -17,3 +17,20 @@ export const TITLES: Record<string, ReactNode> = {
   '/team-modification': <span>👑 팀 수정</span>,
   '/team-calendar': <span>🗓️ 팀 캘린더</span>,
 };
+
+export const FORM_TITLES: Record<
+  string,
+  {
+    title: string;
+    subtitle: string;
+  }
+> = {
+  '/team-registration': {
+    title: '팀 정보 입력하기',
+    subtitle: '팀 생성 후에도 모든 정보를 수정할 수 있어요',
+  },
+  '/team-modification': {
+    title: '팀 정보 수정하기',
+    subtitle: '저장 후에도 모든 정보를 수정할 수 있어요',
+  },
+};
