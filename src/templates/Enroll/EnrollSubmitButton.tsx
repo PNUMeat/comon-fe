3fb386c843a54dd804e393c36a11e6331c186377
JaeAnHan <jaean1999@naver.com>
2024-12-01 16:59:15 +0900

    feat: 프로필 생성 후처리 로직 Enroll 페이지로 이동

--- src/templates/Enroll/EnrollSubmitButton.tsx
+++ src/templates/Enroll/EnrollSubmitButton.tsx
@@ -1,5 +1,7 @@
 import { ComonFormSubmitButton } from '@/components/commons/Form/ComonFormSubmitButton';
 
+import { NavigateFunction, useNavigate } from 'react-router-dom';
+
 import { createProfile } from '@/api/Login';
 import {
   formTextInputAtom,
@@ -9,18 +11,51 @@ import {
 } from '@/store/form';
 import { useAtomValue } from 'jotai';
 
+const parseCookieAsJson = (): Record<string, string> => {
+  return document.cookie
+    .split(';')
+    .map((cookie) => cookie.trim().split('='))
+    .reduce(
+      (acc, [key, value]) => {
+        if (key && value) {
+          acc[key] = value;
+        }
+        return acc;
+      },
+      {} as Record<string, string>
+    );
+};
+
+const postProcessResponse = (navigate: NavigateFunction) => () => {
+  const cookie = parseCookieAsJson();
+  if (Object.entries(cookie).length > 0) {
+    const at = cookie['access_token'];
+    if (at) {
+      localStorage.setItem('Authorization', at);
+      navigate(-1);
+      alert('회원가입에 성공했습니다');
+    }
+  }
+};
+
 export const EnrollSubmitButton = () => {
   const isAllFieldSatisfied = useAtomValue(isEnrollSatisfiedAtom);
   const memberName = useAtomValue(formTextInputAtom);
   const memberExplain = useAtomValue(formTextareaAtom);
   const image = useAtomValue(imageAtom);
+  const navigate = useNavigate();
 
   const onClick = () => {
     createProfile({
       memberName: memberName,
       memberExplain: memberExplain,
       image: image,
-    }).then((res) => console.log(res));
+    })
+      .then(postProcessResponse(navigate))
+      .catch((err) => {
+        console.error(err);
+        alert('회원가입에 실패했습니다');
+      });
   };
 
   return (
--- src/templates/Login/LoginTemplate.tsx
+++ src/templates/Login/LoginTemplate.tsx
@@ -1,43 +1,8 @@
 import { GradientGlassPanel } from '@/components/commons/GradientGlassPanel';
 
-import { useEffect } from 'react';
-
-// import { useNavigate } from 'react-router-dom';
-// import { PATH } from '@/routes/path';
 import { LoginForm } from '@/templates/Login/LoginForm';
 
-const parseCookieAsJson = (): Record<string, string> => {
-  return document.cookie
-    .split(';')
-    .map((cookie) => cookie.trim().split('='))
-    .reduce(
-      (acc, [key, value]) => {
-        if (key && value) {
-          acc[key] = value;
-        }
-        return acc;
-      },
-      {} as Record<string, string>
-    );
-};
-
-// const isNew = (segment: string | null) => {
-//   return segment && segment === 'true';
-// };
-
 export const LoginTemplate = () => {
-  // const navigate = useNavigate();
-  useEffect(() => {
-    const cookie = parseCookieAsJson();
-    if (Object.entries(cookie).length > 0) {
-      const at = cookie['access_token'];
-      if (at) {
-        localStorage.setItem('Authorization', at);
-
-        // navigate(isNew(cookie['new']) ? PATH.ENROLL : PATH.HOME);
-      }
-    }
-  }, []);
   return (
     <GradientGlassPanel>
       <LoginForm h={324} />
