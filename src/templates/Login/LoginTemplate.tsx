f72182d9327a403ad4fb5bb0b24c8f4c8b27688b
JaeAnHan <jaean1999@naver.com>
2024-12-01 16:03:41 +0900

    feat: 카카오 로그인 후 전달 받은 access token 우선 로컬 스토리지에 저장

--- src/templates/Login/LoginTemplate.tsx
+++ src/templates/Login/LoginTemplate.tsx
@@ -1,8 +1,41 @@
 import { GradientGlassPanel } from '@/components/commons/GradientGlassPanel';
 
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { PATH } from '@/routes/path';
 import { LoginForm } from '@/templates/Login/LoginForm';
 
+const parseCookieAsJson = (): Record<string, string> => {
+  return document.cookie
+    .split(';')
+    .map((cookie) => cookie.trim().split('='))
+    .reduce(
+      (acc, [key, value]) => {
+        if (key && value) {
+          acc[key] = value;
+        }
+        return acc;
+      },
+      {} as Record<string, string>
+    );
+};
+
+const isNew = (segment: string | null) => {
+  return segment && segment === 'true';
+};
+
 export const LoginTemplate = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const cookie = parseCookieAsJson();
+    if (cookie) {
+      const at = cookie['access_token'];
+      localStorage.setItem('Authorization', at);
+
+      navigate(isNew(cookie['new']) ? PATH.ENROLL : PATH.HOME);
+    }
+  }, []);
   return (
     <GradientGlassPanel>
       <LoginForm h={324} />
