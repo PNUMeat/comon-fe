6d361c66fbff95bd0cd23c5630ddaac63254eec0
JaeAnHan <jaean1999@naver.com>
2024-12-04 15:38:52 +0900

    feat: 빌드 파일 주석 제거

--- vite.config.ts
+++ vite.config.ts
@@ -27,6 +27,19 @@ export default defineConfig({
       },
     }),
   ],
+  build: {
+    // rollupOptions의 output을 사용하면 번들이 완성 됐을 때, 주석을 제거하지만, terserOptions를 사용하면 압축단계에서 주석을 제거함
+    // 개발 일지 관련 주석 제거가 목적이므로 해당 옵션을 사용해야 빌드 시간이 단축 될 것으로 판단됨 (실험은 안해봄)
+    terserOptions: {
+      format: {
+        comments: false,
+      },
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
