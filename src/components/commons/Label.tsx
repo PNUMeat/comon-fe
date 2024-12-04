d9e8524baa2a817734b81f8572444b32a8c47a40
Jisu Lee <dlwltn0430@naver.com>
2024-12-04 21:57:15 +0900

    feat: Label에 height 추가

--- src/components/commons/Label.tsx
+++ src/components/commons/Label.tsx
@@ -5,10 +5,11 @@ export const Label = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  background: '#f4f4f4';
+  background: #f4f4f4;
   color: ${colors.buttonPurple};
   padding: 2px 20px;
   border: 1px solid ${colors.borderPurple};
   border-radius: 34px;
   box-sizing: border-box;
+  height: 18px;
 `;
