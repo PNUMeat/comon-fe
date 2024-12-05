01a814014ed9e129fafa2963c17272c4cd4924d6
JaeAnHan <jaean1999@naver.com>
2024-12-06 02:52:29 +0900

    chore: imageCompressor 메모 주석 수정

--- src/workers/imageCompressor.ts
+++ src/workers/imageCompressor.ts
@@ -1,6 +1,9 @@
 /**
-  (12/06) JPG만 압축가능한데 다른 확장자도 필요하다면 그냥 browser-image-compression 쓰는게 나을듯함
-  그래도 PNG까지는 해보고 싶음
+  (12/06) JPG만 압축가능한데 JPG는 압축 잘 되는데 PNG는 오히려 크기가 커진다.
+  OffscreenCanvas를 처음써봐서 잘 모르지만, OffscreenCanvas가 문제인가 싶어서
+  다른 레퍼런스들 처럼 그냥 Canvas를 사용해봤는데 이거로 해도 PNG는 그냥 크기가 커짐.
+  그래도 PNG까지는 해보고 싶음.
+  다른 확장자도 필요하다면 그냥 browser-image-compression 쓰는게 나을듯함
  */
 function compressImage(
   image: ImageBitmap,
