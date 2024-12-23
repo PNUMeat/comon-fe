f55172f29e559b5a3ee31982d5d2287180882aef
Jisu Lee <dlwltn0430@naver.com>
2024-12-23 21:23:15 +0900

    refactor: TeamDashboard 컴포넌트 분리

--- src/pages/TeamDashboard/TeamDashboard.tsx
+++ src/pages/TeamDashboard/TeamDashboard.tsx
@@ -1,5 +1,6 @@
+import { CustomCalendar } from '@/components/commons/Calendar/Calendar';
 import { Spacer } from '@/components/commons/Spacer';
-import { CalendarAndPosts } from '@/components/features/TeamDashboard/CalendarAndPosts';
+import { Posts } from '@/components/features/TeamDashboard/Posts';
 import { SidebarAndAnnouncement } from '@/components/features/TeamDashboard/SidebarAndAnnouncement';
 
 import styled from '@emotion/styled';
@@ -10,7 +11,11 @@ export const TeamDashboardPage = () => {
       <Spacer h={28} />
       <Grid>
         <SidebarAndAnnouncement />
-        <CalendarAndPosts />
+        <CalendarSection>
+          <CustomCalendar />
+          <Spacer h={24} />
+          <Posts />
+        </CalendarSection>
       </Grid>
     </>
   );
@@ -25,4 +30,11 @@ const Grid = styled.div`
   grid-template-rows: auto 1fr auto;
   gap: 24px 40px;
   height: 100vh;
+`;
+
+const CalendarSection = styled.section`
+  grid-area: calendar;
+  background-color: #f8f8ff;
+  border-radius: 20px;
+  padding: 20px 36px 40px 36px;
 `;
