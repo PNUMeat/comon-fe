fe283b00feecc316fe565acbf9395b64bf7d42e3
JaeAnHan <jaean1999@naver.com>
2024-12-23 16:30:07 +0900

    feat: 로그인 후 헤더에 팀페이지가 뜨도록 수정

--- src/components/commons/Header.tsx
+++ src/components/commons/Header.tsx
@@ -4,6 +4,7 @@ import { Flex } from '@/components/commons/Flex';
 import { SText } from '@/components/commons/SText';
 import { HeightInNumber } from '@/components/types';
 
+import { useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 import { colors } from '@/constants/colors';
@@ -76,9 +77,12 @@ const ComonLogoWrap = styled.div`
 `;
 
 export const Header: React.FC<HeightInNumber> = ({ h }) => {
-  const isLoggedIn = checkIfLoggedIn();
+  const [isLoggedIn] = useState<boolean>(checkIfLoggedIn());
+  // const isLoggedIn = checkIfLoggedIn();
   const location = useLocation();
   const navigate = useNavigate();
+
+  console.error('???', isLoggedIn);
 
   // TODO: Link를 사용하면 보라색 밑줄이 그여짐
   const onClickHome = () => navigate(PATH.HOME);
@@ -99,7 +103,8 @@ export const Header: React.FC<HeightInNumber> = ({ h }) => {
       </Flex>
       <UserMenu>
         {isLoggedIn ? (
-          <Link to={PATH.PROFILE}>프로필 수정</Link>
+          // <Link to={PATH.PROFILE}>프로필 수정</Link>
+          <Link to={PATH.TEAM_DASHBOARD}>팀 페이지</Link>
         ) : (
           <Link
             to={{
