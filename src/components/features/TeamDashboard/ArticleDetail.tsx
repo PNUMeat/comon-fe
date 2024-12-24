dc2992e15d1bf6fca51a7499a6ce13261209c2f9
Jisu Lee <dlwltn0430@naver.com>
2024-12-24 17:22:59 +0900

    feat: 게시글 상세 조회 API 연결

--- src/components/features/TeamDashboard/ArticleDetail.tsx
+++ src/components/features/TeamDashboard/ArticleDetail.tsx
@@ -1,48 +1,62 @@
 import { Box } from '@/components/commons/Box';
 import { Flex } from '@/components/commons/Flex';
+import { LazyImage } from '@/components/commons/LazyImage';
 import { SText } from '@/components/commons/SText';
 import { Spacer } from '@/components/commons/Spacer';
 
-export const ArticleDetail = () => {
+import { IArticle, IArticlesByDateResponse } from '@/api/dashboard';
+
+interface ArticleDetailProps {
+  data: IArticlesByDateResponse;
+  articleId: number;
+}
+
+export const ArticleDetail: React.FC<ArticleDetailProps> = ({
+  data,
+  articleId,
+}) => {
+  const article = data?.content.find(
+    (item: IArticle) => item.articleId === articleId
+  );
+
   return (
     <Box width="100%" padding="30px 40px">
       <Flex direction="column" justify="center" align="flex-start">
         <SText color="#333" fontSize="24px" fontWeight={700}>
-          게시글
+          {article?.articleTitle}
         </SText>
         <Spacer h={8} />
         <SText color="#777" fontSize="14px" fontWeight={400}>
-          2024.11.01 14:39
+          {article?.createdDate.slice(0, -3)}
         </SText>
         <Spacer h={28} />
         <Flex align="center" gap="8px">
-          <img src="https://via.placeholder.com/16x16" />
-          {/* <LazyImage
-            src="https://via.placeholder.com/16x16"
-            altText="https://via.placeholder.com/600x400"
+          <LazyImage
+            src={article?.memberImage || ''}
+            altText={article?.memberName || ''}
             w={16}
             h={16}
             maxW={16}
-          /> */}
+          />
           <SText color="#333" fontSize="12px" fontWeight={600}>
-            파댕이2
+            {article?.memberName}
           </SText>
         </Flex>
-
         <Spacer h={36} />
-        <img
-          src="https://via.placeholder.com/600x300"
-          style={{ padding: '0px 20px' }}
-        />
-        {/* <LazyImage
-          src="https://via.placeholder.com/600x400"
-          altText="Sample Image"
-          w={100}
-          h={100}
-          maxW={100}
-        /> */}
-        <Spacer h={36} />
-        <div></div>
+        {article?.imageUrl && (
+          <>
+            <LazyImage
+              src={article?.imageUrl}
+              altText="이미지 불러오기 실패"
+              w={600}
+              h={300}
+              maxW={600}
+              style={{ padding: '0px 20px' }}
+            />
+            <Spacer h={36} />
+          </>
+        )}
+        <div>{article?.articleBody}</div>
       </Flex>
     </Box>
   );
