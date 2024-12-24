55c3a4b59ef1456e249b42c023a295ce6a1ae8d1
Jisu Lee <dlwltn0430@naver.com>
2024-12-24 12:55:45 +0900

    feat: 클릭한 날짜의 Tag 표시

--- src/components/features/TeamDashboard/Posts.tsx
+++ src/components/features/TeamDashboard/Posts.tsx
@@ -1,18 +1,36 @@
 import { Box } from '@/components/commons/Box';
 import { Button } from '@/components/commons/Button';
 import { Flex } from '@/components/commons/Flex';
-import { Label } from '@/components/commons/Label';
 import { Pagination } from '@/components/commons/Pagination';
 import { SText } from '@/components/commons/SText';
 import { Spacer } from '@/components/commons/Spacer';
+import { Tag } from '@/components/commons/Tag';
 
 import styled from '@emotion/styled';
 
 interface PostsProps {
+  tags: {
+    subjectDate: string;
+    articleCategory: string;
+  }[];
   selectedDate: string | null;
 }
 
-export const Posts: React.FC<PostsProps> = ({ selectedDate }) => {
+export const Posts: React.FC<PostsProps> = ({ tags, selectedDate }) => {
+  const categoryColors: Record<string, string> = {
+    '스터디 복습': '#6E74FA',
+    '스터디 예습': '#C2C4FB',
+    스터디: '#FFA379',
+    '코딩 테스트': '#FF5780',
+  };
+
+  const getCategoryForSelectedDate = () => {
+    if (!selectedDate) return null;
+    return tags.find((tag) => tag.subjectDate === selectedDate) || null;
+  };
+
+  const category = getCategoryForSelectedDate();
+
   return (
     <div style={{ position: 'relative' }}>
       <Box width="100%" padding="20px 40px" style={{ zIndex: 2 }}>
@@ -21,11 +39,14 @@ export const Posts: React.FC<PostsProps> = ({ selectedDate }) => {
             <SText color="#333" fontSize="24px" fontWeight={700}>
               {selectedDate}
             </SText>
-            <Label background="#FF5780" padding="2px 10px">
-              <SText fontSize="12px" fontWeight={600} color="#fff">
-                코딩테스트
-              </SText>
-            </Label>
+            {category && (
+              <Tag
+                bgColor={categoryColors[category.articleCategory]}
+                label={category.articleCategory}
+                padding="4px 10px"
+                fontSize="10px"
+              />
+            )}
           </Flex>
           <Button padding="8px 14px">주제 확인하기</Button>
         </Flex>
