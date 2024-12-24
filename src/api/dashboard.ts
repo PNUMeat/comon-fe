0fae4eb6d292b4ef53dd9e8f12bb434a607028f0
Jisu Lee <dlwltn0430@naver.com>
2024-12-24 14:21:50 +0900

    feat: 특정 날짜 게시글 조회 요청 함수 구현

--- src/api/dashboard.ts
+++ src/api/dashboard.ts
@@ -13,6 +13,26 @@ export interface ITeamInfoAndTagsResponse {
   subjectArticleDateAndTagResponses: ICalendarTag[];
 }
 
+export interface IArticle {
+  articleId: number;
+  articleTitle: string;
+  articleBody: string;
+  createdDate: string;
+  imageUrl: string;
+  memberName: string;
+  memberImage: string;
+}
+
+export interface IArticlesByDateResponse {
+  articleInformation: IArticle[];
+  page: {
+    size: number;
+    number: number;
+    totalElements: number;
+    totalPages: number;
+  };
+}
+
 export const getTeamInfoAndTags = async (
   teamId: number,
   year: number,
@@ -21,6 +41,19 @@ export const getTeamInfoAndTags = async (
   const res = await apiInstance.get<ServerResponse<ITeamInfoAndTagsResponse>>(
     `/v1/teams/${teamId}/team-page`,
     { params: { year, month } }
+  );
+
+  return res.data.data;
+};
+
+export const getArticlesByDate = async (
+  teamId: number,
+  date: string,
+  page: number
+): Promise<IArticlesByDateResponse> => {
+  const res = await apiInstance.get<ServerResponse<IArticlesByDateResponse>>(
+    `/v1/articles/${teamId}/by-date`,
+    { params: { date, page } }
   );
 
   return res.data.data;
