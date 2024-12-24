3130b80d1b7687fa10b3cf79a53a79f56d649474
Jisu Lee <dlwltn0430@naver.com>
2024-12-24 15:33:30 +0900

    design: 주제 확인 Box 퍼블리싱

--- src/components/features/TeamDashboard/TopicDetail.tsx
+++ src/components/features/TeamDashboard/TopicDetail.tsx
@@ -1,41 +1,60 @@
 import { Box } from '@/components/commons/Box';
-import { LazyImage } from '@/components/commons/LazyImage';
+import { Flex } from '@/components/commons/Flex';
 import { SText } from '@/components/commons/SText';
 import { Spacer } from '@/components/commons/Spacer';
 
+import AnnouncementIcon from '@/assets/TeamDashboard/announcement.png';
 import styled from '@emotion/styled';
 
 export const TopicDetail = () => {
   return (
-    <TopicDetailWrapper>
-      <Box width="100%" padding="20px">
-        <SText color="#333" fontSize="24px" fontWeight={700}>
-          11/26 코테 풀이
-        </SText>
+    <Box width="100%" padding="30px 40px">
+      <Flex direction="column" justify="center" align="flex-start">
+        <Flex align="center" gap="8px">
+          <Icon src={AnnouncementIcon} />
+          <SText color="#333" fontSize="24px" fontWeight={700}>
+            11/26 코테 풀이
+          </SText>
+        </Flex>
         <Spacer h={8} />
-        <SText color="#777" fontSize="12px" fontWeight={400}>
+        <SText color="#777" fontSize="14px" fontWeight={400}>
           2024.11.01 14:39
         </SText>
-        <Spacer h={12} />
-        <LazyImage
-          src="https://via.placeholder.com/600x400" // 이미지 URL 대체
+        <Spacer h={28} />
+        <Flex align="center" gap="8px">
+          <img src="https://via.placeholder.com/16x16" />
+          {/* <LazyImage
+            src="https://via.placeholder.com/16x16"
+            altText="https://via.placeholder.com/600x400"
+            w={16}
+            h={16}
+            maxW={16}
+          /> */}
+          <SText color="#333" fontSize="12px" fontWeight={600}>
+            파댕이2
+          </SText>
+        </Flex>
+
+        <Spacer h={36} />
+        <img
+          src="https://via.placeholder.com/600x300"
+          style={{ padding: '0px 30px' }}
+        />
+        {/* <LazyImage
+          src="https://via.placeholder.com/600x400"
           altText="Sample Image"
           w={100}
           h={100}
           maxW={100}
-        />
-        <Spacer h={12} />
-        <SText color="#333" fontSize="14px" fontWeight={400}>
-          게시물 내용 상세 설명
-        </SText>
-      </Box>
-    </TopicDetailWrapper>
+        /> */}
+        <Spacer h={36} />
+        <div></div>
+      </Flex>
+    </Box>
   );
 };
 
-const TopicDetailWrapper = styled.div`
-  background: white;
-  border-radius: 8px;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  padding: 20px;
+const Icon = styled.img`
+  width: 24px;
+  height: 24px;
 `;
