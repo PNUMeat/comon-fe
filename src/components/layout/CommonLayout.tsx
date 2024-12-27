2509cd4c7806af2b0efc59f364448f4936dc2c4e
JaeAnHan <jaean1999@naver.com>
2024-12-27 16:01:29 +0900

    feat: 메인페이지 페이지 이동시 스크롤 탑

--- src/components/layout/CommonLayout.tsx
+++ src/components/layout/CommonLayout.tsx
@@ -1,13 +1,19 @@
 import { Container } from '@/components/commons/Container';
 import { Header } from '@/components/commons/Header';
 
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const headerHeight = 72;
 
 export const CommonLayout: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+  }, [location.pathname]);
+
   return (
     <Container padding={'0'}>
       <Header h={headerHeight} />
