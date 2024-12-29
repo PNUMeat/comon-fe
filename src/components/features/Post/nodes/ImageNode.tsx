3049f646855aef0c0727eb58e967708cee41cac1
JaeAnHan <jaean1999@naver.com>
2024-12-29 21:37:01 +0900

    feat: 이미지 maxWidth 600으로 변경

--- src/components/features/Post/nodes/ImageNode.tsx
+++ src/components/features/Post/nodes/ImageNode.tsx
@@ -166,7 +166,7 @@ export class ImageNode extends DecoratorNode<JSX.Element> {
 export const $createImageNode = ({
   altText,
   height,
-  maxWidth = 500,
+  maxWidth = 600,
   src,
   width,
   key,
