b757e3f2fd89aab79fa25387f35436444b73a9ac
JaeAnHan <jaean1999@naver.com>
2024-12-31 22:36:41 +0900

    fix: 버튼 key 추가

--- src/api/team.ts
+++ src/api/team.ts
@@ -96,19 +96,285 @@ export const createTeam = async ({
   return res.data.data;
 };
 
+const res = {
+  status: 'success',
+  code: 200,
+  message: null,
+  data: {
+    myTeams: [
+      {
+        teamId: 16,
+        teamName: 'aa',
+        teamExplain: '11',
+        imageUrl:
+          'https://pnu-comon-s3-bucket.s3.ap-northeast-2.amazonaws.com/team/default-image.png',
+        topic: '스터디',
+        memberLimit: 12,
+        memberCount: 1,
+        streakDays: 0,
+        successMemberCount: 0,
+        teamAnnouncement: '',
+        createdAt: '2024-12-26',
+      },
+      {
+        teamId: 15,
+        teamName: 'scroll',
+        teamExplain: 'scsc',
+        imageUrl:
+          'https://pnu-comon-s3-bucket.s3.ap-northeast-2.amazonaws.com/team/default-image.png',
+        topic: '스터디',
+        memberLimit: 12,
+        memberCount: 1,
+        streakDays: 0,
+        successMemberCount: 0,
+        teamAnnouncement: '',
+        createdAt: '2024-12-26',
+      },
+      {
+        teamId: 14,
+        teamName: 'asd',
+        teamExplain: 'qqqq',
+        imageUrl:
+          'https://pnu-comon-s3-bucket.s3.ap-northeast-2.amazonaws.com/team/default-image.png',
+        topic: '코딩테스트',
+        memberLimit: 12,
+        memberCount: 1,
+        streakDays: 0,
+        successMemberCount: 0,
+        teamAnnouncement: '',
+        createdAt: '2024-12-26',
+      },
+      {
+        teamId: 13,
+        teamName: 'asd',
+        teamExplain: 'qqqq',
+        imageUrl:
+          'https://pnu-comon-s3-bucket.s3.ap-northeast-2.amazonaws.com/team/default-image.png',
+        topic: '코딩테스트',
+        memberLimit: 12,
+        memberCount: 1,
+        streakDays: 0,
+        successMemberCount: 0,
+        teamAnnouncement: '',
+        createdAt: '2024-12-26',
+      },
+      {
+        teamId: 10,
+        teamName: 'Team QA',
+        teamExplain: 'qa',
+        imageUrl:
+          'https://pnu-comon-s3-bucket.s3.ap-northeast-2.amazonaws.com/team/default-image.png',
+        topic: '스터디',
+        memberLimit: 5,
+        memberCount: 1,
+        streakDays: 0,
+        successMemberCount: 0,
+        teamAnnouncement: '',
+        createdAt: '2024-12-27',
+      },
+      {
+        teamId: 3,
+        teamName: '파댕이 밥주기',
+        teamExplain: '파댕이 밥',
+        imageUrl:
+          'https://pnu-comon-s3-bucket.s3.ap-northeast-2.amazonaws.com/team/bae6cb5b-2772-41a7-b56f-808766d0dc50',
+        topic: '코딩테스트',
+        memberLimit: 10,
+        memberCount: 6,
+        streakDays: 0,
+        successMemberCount: 0,
+        teamAnnouncement: '공지수정222',
+        createdAt: '2024-12-23',
+      },
+      {
+        teamId: 2,
+        teamName: '장진영팀',
+        teamExplain: '코딩테스트 입니당~~',
+        imageUrl:
+          'https://pnu-comon-s3-bucket.s3.ap-northeast-2.amazonaws.com/team/default-image.png',
+        topic: '코딩테스트',
+        memberLimit: 11,
+        memberCount: 3,
+        streakDays: 0,
+        successMemberCount: 0,
+        teamAnnouncement: '안녕하세요 공지입니다.',
+        createdAt: '2024-12-22',
+      },
+      {
+        teamId: 1,
+        teamName: '코딩테스트트트',
+        teamExplain: '코딩테스트 입니당~~',
+        imageUrl:
+          'https://pnu-comon-s3-bucket.s3.ap-northeast-2.amazonaws.com/team/default-image.png',
+        topic: '코딩테스트',
+        memberLimit: 11,
+        memberCount: 4,
+        streakDays: 0,
+        successMemberCount: 0,
+        teamAnnouncement: '하이하이하이하이',
+        createdAt: '2024-12-22',
+      },
+    ],
+    allTeams: {
+      content: [
+        {
+          teamId: 17,
+          teamName: '한재안',
+          teamExplain: '한재안',
+          imageUrl:
+            'https://pnu-comon-s3-bucket.s3.ap-northeast-2.amazonaws.com/team/default-image.png',
+          topic: '스터디',
+          memberLimit: 3,
+          memberCount: 2,
+          streakDays: 0,
+          createdAt: '2024-12-27',
+          members: [
+            {
+              memberName: 'ddy',
+              imageUrl:
+                'https://pnu-comon-s3-bucket.s3.ap-northeast-2.amazonaws.com/profile/default-image.png',
+              memberExplain: 'hjjuiiiuououououo',
+              uuid: '51021d92-eefe-41a7-962a-d0fc029bb866',
+            },
+            {
+              memberName: '김민욱',
+              imageUrl:
+                'https://pnu-comon-s3-bucket.s3.ap-northeast-2.amazonaws.com/profile/default-image.png',
+              memberExplain: '.',
+              uuid: '11114201-2411-48cf-a6aa-e6de4607956d',
+            },
+          ],
+        },
+        {
+          teamId: 12,
+          teamName: '티리티리팀',
+          teamExplain: '티리티리팀이에요',
+          imageUrl:
+            'https://pnu-comon-s3-bucket.s3.ap-northeast-2.amazonaws.com/team/default-image.png',
+          topic: '코딩테스트',
+          memberLimit: 5,
+          memberCount: 1,
+          streakDays: 0,
+          createdAt: '2024-12-27',
+          members: [
+            {
+              memberName: null,
+              imageUrl:
+                'https://pnu-comon-s3-bucket.s3.ap-northeast-2.amazonaws.com/profile/default-image.png',
+              memberExplain: null,
+              uuid: '358e0577-696f-418a-a0fc-80d63dbfe587',
+            },
+          ],
+        },
+        {
+          teamId: 11,
+          teamName: '티리티리팀',
+          teamExplain: '티리티리팀이에요',
+          imageUrl:
+            'https://pnu-comon-s3-bucket.s3.ap-northeast-2.amazonaws.com/team/default-image.png',
+          topic: '코딩테스트',
+          memberLimit: 5,
+          memberCount: 1,
+          streakDays: 0,
+          createdAt: '2024-12-27',
+          members: [
+            {
+              memberName: null,
+              imageUrl:
+                'https://pnu-comon-s3-bucket.s3.ap-northeast-2.amazonaws.com/profile/default-image.png',
+              memberExplain: null,
+              uuid: '358e0577-696f-418a-a0fc-80d63dbfe587',
+            },
+          ],
+        },
+        {
+          teamId: 10,
+          teamName: 'Team QA',
+          teamExplain: 'qa',
+          imageUrl:
+            'https://pnu-comon-s3-bucket.s3.ap-northeast-2.amazonaws.com/team/default-image.png',
+          topic: '스터디',
+          memberLimit: 5,
+          memberCount: 1,
+          streakDays: 0,
+          createdAt: '2024-12-27',
+          members: [
+            {
+              memberName: '하내안',
+              imageUrl:
+                'https://pnu-comon-s3-bucket.s3.ap-northeast-2.amazonaws.com/profile/f8f43af8-22c1-4885-a580-1f8233eec3d7',
+              memberExplain: '하내안입니다',
+              uuid: '495892de-1014-4987-b9ea-5137efeefc84',
+            },
+          ],
+        },
+        {
+          teamId: 9,
+          teamName: '현빈 테스트',
+          teamExplain: '기모띵',
+          imageUrl:
+            'https://pnu-comon-s3-bucket.s3.ap-northeast-2.amazonaws.com/team/default-image.png',
+          topic: '코딩테스트',
+          memberLimit: 4,
+          memberCount: 1,
+          streakDays: 0,
+          createdAt: '2024-12-27',
+          members: [
+            {
+              memberName: 'ddy',
+              imageUrl:
+                'https://pnu-comon-s3-bucket.s3.ap-northeast-2.amazonaws.com/profile/default-image.png',
+              memberExplain: 'hjjuiiiuououououo',
+              uuid: '51021d92-eefe-41a7-962a-d0fc029bb866',
+            },
+          ],
+        },
+        {
+          teamId: 16,
+          teamName: 'aa',
+          teamExplain: '11',
+          imageUrl:
+            'https://pnu-comon-s3-bucket.s3.ap-northeast-2.amazonaws.com/team/default-image.png',
+          topic: '스터디',
+          memberLimit: 12,
+          memberCount: 1,
+          streakDays: 0,
+          createdAt: '2024-12-26',
+          members: [
+            {
+              memberName: '하내안',
+              imageUrl:
+                'https://pnu-comon-s3-bucket.s3.ap-northeast-2.amazonaws.com/profile/f8f43af8-22c1-4885-a580-1f8233eec3d7',
+              memberExplain: '하내안입니다',
+              uuid: '495892de-1014-4987-b9ea-5137efeefc84',
+            },
+          ],
+        },
+      ],
+      page: {
+        size: 6,
+        number: 0,
+        totalElements: 16,
+        totalPages: 3,
+      },
+    },
+  },
+};
+
 export const getTeamList = async (
   sort: string = 'recent',
   page: number = 0,
   size: number = 6
 ): Promise<ITeamListResponse> => {
-  const res = await apiInstance.get<ServerResponse<ITeamListResponse>>(
-    `/v1/teams/combined`,
-    {
-      params: { sort, page, size },
-    }
-  );
-
-  return res.data.data;
+  // const res = await apiInstance.get<ServerResponse<ITeamListResponse>>(
+  //   `/v1/teams/combined`,
+  //   {
+  //     params: { sort, page, size },
+  //   }
+  // );
+  //
+  // return res.data.data;
+  return res.data;
 };
 
 type TeamJoinResp = {
