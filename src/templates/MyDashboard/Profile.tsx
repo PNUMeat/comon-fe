2f265adc94837a3d1f40a3a1dff978b4e83d80cf
JaeAnHan <jaean1999@naver.com>
2025-01-01 22:55:40 +0900

    feat: 마이 페이지 계정 탈퇴 페이지 추가 및 연동

--- src/templates/MyDashboard/Profile.tsx
+++ src/templates/MyDashboard/Profile.tsx
@@ -5,28 +5,32 @@ import { SText } from '@/components/commons/SText';
 
 import { Fragment, Suspense, useState } from 'react';
 
-import { changeProfile, getMyProfile } from '@/api/user';
+import { changeProfile, getMyProfile, withdrawMember } from '@/api/user';
+import comon from '@/assets/Home/comon500x500.png';
+import Alarm from '@/assets/Withdraw/alarm.svg';
 import styled from '@emotion/styled';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 
 // margin-bottom이 하단 여백
-const ProfileWrap = styled.div`
+const ProfileWrap = styled.div<{ doBlur: boolean }>`
   box-sizing: border-box;
   margin-bottom: 400px;
   display: flex;
   position: relative;
+  align-items: flex-end;
+  background-color: ${(props) =>
+    props.doBlur
+      ? 'rgba(255, 255, 255, 0.3); backdrop-filter: blur(20px);'
+      : '#fff'};
 
   width: 700px;
   min-height: 370px;
   padding: 32px 51px 28px 51px;
   flex-direction: column;
-  align-items: flex-end;
   gap: 8px;
   flex-shrink: 0;
   border-radius: 20px;
   border: 1px solid #8488ec;
-
-  background: #fff;
 
   box-shadow: 5px 7px 11.6px 0px rgba(63, 63, 77, 0.07);
   backdrop-filter: blur(20px);
@@ -42,7 +46,7 @@ const SubHeader = styled.div`
   leading-trim: both;
 
   text-edge: cap;
-  font-family: 'Pretendard Variable';
+  font-family: 'Pretendard';
   font-size: 20px;
   font-style: normal;
   font-weight: 700;
@@ -56,7 +60,7 @@ const Heading = styled.div`
   leading-trim: both;
 
   text-edge: cap;
-  font-family: 'Pretendard Variable';
+  font-family: 'Pretendard';
   font-size: 20px;
   font-style: normal;
   font-weight: 700;
@@ -76,7 +80,7 @@ const PInfoLabel = styled.div`
   color: #333;
   leading-trim: both;
   text-edge: cap;
-  font-family: 'Pretendard Variable';
+  font-family: 'Pretendard';
   font-size: 16px;
   font-style: normal;
   font-weight: 500;
@@ -87,7 +91,7 @@ const ProfileTextValue = styled.div<{ fontWeight: number }>`
   color: #333;
   leading-trim: both;
   text-edge: cap;
-  font-family: 'Pretendard Variable';
+  font-family: 'Pretendard';
   font-size: 16px;
   font-style: normal;
   font-weight: ${(props) => props.fontWeight};
@@ -108,7 +112,7 @@ const ModifyButton = styled.button`
   color: #fff;
 
   text-align: center;
-  font-family: 'Pretendard Variable';
+  font-family: 'Pretendard';
   font-size: 14px;
   font-style: normal;
   font-weight: 500;
@@ -127,7 +131,7 @@ const TextInput = styled.input<{ fontWeight: number }>`
   color: #333;
   leading-trim: both;
   text-edge: cap;
-  font-family: 'Pretendard Variable';
+  font-family: 'Pretendard';
   font-size: 16px;
   font-style: normal;
   font-weight: ${(props) => props.fontWeight};
@@ -149,12 +153,29 @@ const ProfileForm = styled.form`
   align-items: flex-end;
 `;
 
-const WithdrawButton = styled.button`
+const WithdrawButtonWrap = styled.button`
   width: 89px;
   position: absolute;
   bottom: 0;
   transform: translate(9px, 40px);
 `;
+
+const WithdrawButton: React.FC<{
+  setMode: React.Dispatch<React.SetStateAction<string>>;
+}> = ({ setMode }) => {
+  return (
+    <WithdrawButtonWrap onClick={() => setMode('withdraw')}>
+      <SText
+        color={'#777'}
+        fontFamily={'Pretendard'}
+        fontSize={'14px'}
+        fontWeight={500}
+      >
+        계정 탈퇴 {'>'}
+      </SText>
+    </WithdrawButtonWrap>
+  );
+};
 
 // const profileModes = ['query', 'modify', 'withdraw'];
 
@@ -201,8 +222,10 @@ export const Profile = () => {
         <span>📋</span>
         <span> {mode === 'withdraw' ? '회원탈퇴' : '프로필'}</span>
       </SubHeader>
-      <ProfileWrap>
-        <Heading>내 프로필</Heading>
+      <ProfileWrap doBlur={mode === 'withdraw'}>
+        {mode !== 'withdraw' && <Heading>내 프로필</Heading>}
+
+        {mode === 'withdraw' && <WithdrawTemplate setMode={setMode} />}
         {mode === 'modify' && (
           <ProfileForm onSubmit={handleSubmit}>
             <ProfileModifier {...data} />
@@ -217,16 +240,7 @@ export const Profile = () => {
             </ModifyButton>
           </Fragment>
         )}
-        <WithdrawButton onClick={() => setMode('withdraw')}>
-          <SText
-            color={'#777'}
-            fontFamily={'Pretendard Variable'}
-            fontSize={'14px'}
-            fontWeight={500}
-          >
-            계정 탈퇴 {'>'}
-          </SText>
-        </WithdrawButton>
+        {mode !== 'withdraw' && <WithdrawButton setMode={setMode} />}
       </ProfileWrap>
     </Flex>
   );
@@ -292,3 +306,196 @@ const ProfileModifier: React.FC<{
     />
   </ProfileInfoGrid>
 );
+
+const WithdrawTemplateWrapper = styled.div`
+  margin: 50px auto;
+  width: 334px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const LittleComonLogo = styled.img`
+  width: 126px;
+  height: 126px;
+  object-fit: contain;
+  border-radius: 50%;
+  margin-bottom: 11px;
+`;
+
+const WithdrawAlarm = styled.div`
+  margin-bottom: 42px;
+  display: flex;
+  gap: 10px;
+  width: 100%;
+  justify-content: center;
+`;
+
+const WithdrawLabel = styled.div`
+  width: 181px;
+  height: 38px;
+`;
+
+const Desc = styled.div`
+  margin-top: 26px;
+  margin-bottom: 26px;
+  text-align: center;
+`;
+
+const StrongWrapper = styled.div`
+  margin-bottom: 48px;
+  height: 64px;
+  box-sizing: border-box;
+  padding: 0 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+  justify-content: center;
+
+  ul li {
+    color: #9e2a2f;
+    font-size: 14px;
+    line-height: 1.5;
+    position: relative;
+    padding-left: 12px;
+    white-space: nowrap;
+    line-height: 14px;
+    margin: 8px 0;
+  }
+
+  ul li::before {
+    content: '•';
+    color: #9e2a2f;
+    font-size: 20px;
+    position: absolute;
+    left: 0;
+    top: -2px;
+  }
+`;
+
+const AgreementWrap = styled.div`
+  margin-bottom: 32px;
+  display: flex;
+  gap: 10px;
+  height: 22px;
+  align-items: center;
+`;
+
+const SubmitWithdrawButton = styled.button<{ isEnabled: boolean }>`
+  margin-bottom: 21px;
+  display: flex;
+  width: 148px;
+  height: 38px;
+  padding: 9px 46px;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  border-radius: 24px;
+  border: 1px solid #919191;
+
+  background: ${(props) => (props.isEnabled ? '#EF2528' : '#e4e4e4')};
+`;
+
+const WithdrawTemplate: React.FC<{
+  setMode: React.Dispatch<React.SetStateAction<string>>;
+}> = ({ setMode }) => {
+  const onClickWithdraw = () =>
+    withdrawMember()
+      .then(() => {
+        sessionStorage.removeItem('Authorization');
+        alert('계정 탈퇴에 성공했습니다.');
+        window.location.href = '/';
+      })
+      .catch(() => alert('계정 탈퇴 요청에 실패했습니다.'));
+
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(event.target.checked);
+  };
+
+  return (
+    <WithdrawTemplateWrapper>
+      <LittleComonLogo src={comon} />
+      <WithdrawAlarm>
+        <img src={Alarm} width={'36px'} height={'30px'} alt={'warning image'} />
+        <WithdrawLabel>
+          <SText
+            fontSize={'32px'}
+            fontWeight={700}
+            fontFamily={'Pretendard'}
+            whiteSpace={'nowrap'}
+          >
+            회원 탈퇴 알림
+          </SText>
+        </WithdrawLabel>
+      </WithdrawAlarm>
+      <SText
+        fontSize={'24px'}
+        fontWeight={500}
+        fontFamily={'Pretendard'}
+        whiteSpace={'nowrap'}
+        style={{
+          textDecorationLine: 'underline',
+          textDecorationStyle: 'solid',
+        }}
+      >
+        코몬 탈퇴 전에 꼭 확인하세요
+      </SText>
+      <Desc>
+        <SText fontFamily={'Pretendard'} fontSize={'15px'} fontWeight={500}>
+          탈퇴하시면 이용 중인 계정이 폐쇄되며 <br /> 모든 데이터는 복구가
+          불가능합니다.
+        </SText>
+      </Desc>
+      <StrongWrapper>
+        <ul>
+          <li>게시글, 프로필 등 모든 정보가 삭제됩니다.</li>
+          <li>이후에는 모든정보에 접근이 불가능합니다.</li>
+        </ul>
+      </StrongWrapper>
+      <AgreementWrap>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <SText
+          color={'#777'}
+          fontSize={'16px'}
+          fontWeight={500}
+          fontFamily={'Pretendard'}
+          whiteSpace={'nowrap'}
+        >
+          안내사항을 모두 확인하였으며, 이에 동의합니다.
+        </SText>
+      </AgreementWrap>
+      <SubmitWithdrawButton
+        isEnabled={isChecked}
+        disabled={!isChecked}
+        onClick={onClickWithdraw}
+      >
+        <SText
+          whiteSpace={'nowrap'}
+          color={isChecked ? '#fff' : '#919191'}
+          fontSize={'16px'}
+          fontWeight={500}
+          fontFamily={'Pretendard'}
+        >
+          탈퇴하기
+        </SText>
+      </SubmitWithdrawButton>
+      <div onClick={() => setMode('query')}>
+        <SText
+          color={'#777'}
+          fontFamily={'Pretendard'}
+          fontSize={'14px'}
+          fontWeight={500}
+        >
+          {'<'} 프로필로 돌아가기
+        </SText>
+      </div>
+    </WithdrawTemplateWrapper>
+  );
+};
