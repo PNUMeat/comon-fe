7f6c068e951803997208a96a5d634706c0885d79
mingkyeongg <ming0820@naver.com>
2025-01-04 15:24:53 +0900

    style: event floating button 스타일

--- src/components/commons/EventFloating/EventFloatingButton.tsx
+++ src/components/commons/EventFloating/EventFloatingButton.tsx
@@ -1,7 +1,32 @@
 import styled from '@emotion/styled';
+import ButtonIcon from '@/assets/EventFloating/ButtonIcon.svg';
+
+const FloatingButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: linear-gradient(90deg, #FFD482 0%, #FF377F 100%);
+  width: 65px;
+  height: 65px;
+  border-radius: 50%;
+`;
+
+const FloatingButtonStyle = styled.button`
+width: 60px;
+height: 60px;
+border-radius: 50%;
+background: #6E74FA;
+background-image: url(${ButtonIcon});
+background-repeat: no-repeat;
+background-position: center;
+`;
+
+
 
 export const EventFloatingButton: React.FC = () => {
   return (
-    <button>Button</button>
+    <FloatingButtonContainer>
+      <FloatingButtonStyle />
+    </FloatingButtonContainer>
   );
 }
