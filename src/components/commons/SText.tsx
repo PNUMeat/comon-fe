37900576d7e93bf312f887793bffb297db64c72e
JaeAnHan <jaean1999@naver.com>
2025-01-07 01:44:55 +0900

    feat: SText 2줄 이상이면 ... 표기, 1줄 이상일 때 줄 간격 4px 추가

--- src/components/commons/SText.tsx
+++ src/components/commons/SText.tsx
@@ -17,10 +17,25 @@ export const SText = styled.div<TextProps>`
   font-family: ${(props) => props.fontFamily || 'inherit'};
   font-size: ${(props) => props.fontSize || 'inherit'};
   font-weight: ${(props) => props.fontWeight || 'inherit'};
-  line-height: ${(props) =>
-    props.lineHeight ? props.lineHeight : (props?.fontSize ?? 'inherit')};
+  line-height: ${(props) => {
+    if (props.lineHeight) return props.lineHeight;
+
+    if (props.fontSize?.endsWith('px')) {
+      const fontSizeValue = parseInt(props.fontSize, 10);
+      return `${fontSizeValue + 4}px`;
+    }
+
+    return 'inherit';
+  }};
   letter-spacing: ${(props) => props.letterSpacing || 'inherit'};
   text-align: ${(props) => props.textAlign || 'inherit'};
   white-space: ${(props) => props.whiteSpace || 'inherit'};
   opacity: ${(props) => props.opacity || 'inherit'};
+
+  width: 100%;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
--- src/store/dashboard.ts
+++ src/store/dashboard.ts
@@ -3,7 +3,6 @@ import { atom } from 'jotai';
 export const selectedPostIdAtom = atom<number | null>(null);
 export const currentViewAtom = atom<string | null>(null);
 export const selectedDateAtom = atom<string>(
-  // new Date().toISOString().split('T')[0]
   new Date().toLocaleDateString('en-CA', { timeZone: 'Asia/Seoul' })
 );
 export const pageAtom = atom<number>(0);
