4393c14d044a715627c2a327d69972888cfb23d0
JaeAnHan <jaean1999@naver.com>
2025-01-08 04:56:57 +0900

    feat: 사진 resizer 추가하지 않을 계획이라 사전 작업 제거

--- src/components/features/Post/nodes/LazyEditorImage.tsx
+++ src/components/features/Post/nodes/LazyEditorImage.tsx
@@ -1,22 +1,14 @@
 import { ILazyImage, LazyImage } from '@/components/commons/LazyImage';
 import { $isImageNode } from '@/components/features/Post/nodes/ImageNode';
 
-import React, {
-  Suspense,
-  useCallback,
-  useEffect,
-  useRef,
-  useState,
-} from 'react';
+import React, { Suspense, useCallback, useEffect, useRef } from 'react';
 
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import useLexicalEditable from '@lexical/react/useLexicalEditable';
 import { useLexicalNodeSelection } from '@lexical/react/useLexicalNodeSelection';
 import { mergeRegister } from '@lexical/utils';
 import {
-  // $getNodeByKey,
-  $getSelection, // $isNodeSelection,
-  BaseSelection,
+  $getSelection,
   CLICK_COMMAND,
   COMMAND_PRIORITY_LOW,
   KEY_BACKSPACE_COMMAND,
@@ -33,20 +25,14 @@ const LazyEditorImage: React.FC<ILazyImage> = ({
 }) => {
   const [editor] = useLexicalComposerContext();
   const isEditable = useLexicalEditable();
-  // const editorRootElement = editor.getRootElement();
   const [isSelected, setSelected, clearSelection] = useLexicalNodeSelection(
     nodeKey as string
   );
 
   const imageRef = useRef<HTMLImageElement | null>(null);
-  const [isResizing, setIsResizing] = useState(false);
-  const [selection, setSelection] = useState<BaseSelection | null>(null);
-  const isFocused = (isSelected || isResizing) && isEditable;
-  // const isFocused = isSelected && isEditable;
+  const isFocused = isSelected && isEditable;
 
-  console.log(setIsResizing, selection);
-
-  // TODO : selection이 커서 및 클릭 기반으로 변경되기 때문에 삭제 버튼을 만들기가 까다로움
+  // TODO: selection이 커서 및 클릭 기반으로 변경되기 때문에 삭제 버튼을 만들기가 까다로움
   const deleteImage = useCallback(
     (payload: KeyboardEvent) => {
       const deleteSelection = $getSelection();
@@ -69,10 +55,6 @@ const LazyEditorImage: React.FC<ILazyImage> = ({
 
   const onClick = useCallback(
     (event: MouseEvent) => {
-      if (isResizing) {
-        return true;
-      }
-
       if (event.target === imageRef.current) {
         clearSelection();
         setSelected(true);
@@ -82,17 +64,11 @@ const LazyEditorImage: React.FC<ILazyImage> = ({
 
       return false;
     },
-    [isResizing, isSelected, setSelected, clearSelection]
+    [isSelected, setSelected, clearSelection]
   );
 
   useEffect(() => {
-    let isMounted = true;
     const unregister = mergeRegister(
-      editor.registerUpdateListener(({ editorState }) => {
-        if (isMounted) {
-          setSelection(editorState.read(() => $getSelection()));
-        }
-      }),
       editor.registerCommand<MouseEvent>(
         CLICK_COMMAND,
         onClick,
@@ -111,80 +87,35 @@ const LazyEditorImage: React.FC<ILazyImage> = ({
     );
 
     return () => {
-      isMounted = false;
       unregister();
     };
   }, [editor]);
 
-  // const onResizeStart = (direction: number) => {
-  //   if (editorRootElement !== null) {
-  //     editorRootElement.style.setProperty(
-  //       'cursor',
-  //       `${getDirectionStr(direction)}-resize`,
-  //       'important'
-  //     );
-  //   }
-  //   setIsResizing(true);
-  // };
-  //
-  // const onResizeEnd = (
-  //   nextWidth: 'inherit' | number,
-  //   nextHeight: 'inherit' | number
-  // ) => {
-  //   if (editorRootElement !== null) {
-  //     editorRootElement.style.setProperty('cursor', 'text');
-  //   }
-  //   setIsResizing(false);
-  //   editor.update(() => {
-  //     const node = $getNodeByKey(nodeKey);
-  //     if ($isImageNode(node)) {
-  //       node.setWidthAndHeight(nextWidth, nextHeight);
-  //     }
-  //   });
-  // };
-
-  // const maxWImg = maxW
-  //   ? maxW
-  //   : editorRootElement !== null
-  //     ? editorRootElement.getBoundingClientRect().width - 20
-  //     : 100;
-  // const maxHImg =
-  //   editorRootElement !== null
-  //     ? editorRootElement.getBoundingClientRect().height - 20
-  //     : 100;
-
   return (
     <Suspense fallback={null}>
-      {isFocused && (
-        <div
-          style={{
-            color: 'white',
-            backgroundColor: 'black',
-            position: 'absolute',
-          }}
-        >
-          backspace 또는 del 키를 통해 삭제
-        </div>
-      )}
-      <LazyImage
-        altText={altText}
-        w={w}
-        maxW={maxW}
-        h={h}
-        src={src}
-        className={isFocused ? 'focused' : undefined}
-        imageRef={imageRef}
-        nodeKey={nodeKey}
-      />
-      {/*{isFocused && $isNodeSelection(selection) && (*/}
-      {/*    <ImageResizer*/}
-      {/*        imageRef={imageRef}*/}
-      {/*        maxW={maxWImg}*/}
-      {/*        maxH={maxHImg}*/}
-      {/*        onResizeStart={onResizeStart}*/}
-      {/*        onResizeEnd={onResizeEnd}*/}
-      {/*    />*/}
-      {/*)}*/}
+      <div style={{ position: 'relative' }}>
+        {isFocused && (
+          <div
+            style={{
+              color: 'white',
+              backgroundColor: 'black',
+              position: 'absolute',
+            }}
+          >
+            backspace 또는 del 키를 통해 삭제
+          </div>
+        )}
+        <LazyImage
+          altText={altText}
+          w={w}
+          maxW={maxW}
+          h={h}
+          src={src}
+          className={isFocused ? 'focused' : undefined}
+          imageRef={imageRef}
+          nodeKey={nodeKey}
+        />
+      </div>
     </Suspense>
   );
 };
