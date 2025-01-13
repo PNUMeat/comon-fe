9dfd0a28a8aabe91a876b2da05443289e7b4b466
JaeAnHan <jaean1999@naver.com>
2025-01-13 23:51:38 +0900

    feat: Spacer 컴포넌트 isRef 프롭스 추가

--- src/components/commons/Spacer.tsx
+++ src/components/commons/Spacer.tsx
@@ -6,9 +6,11 @@ export const Spacer = styled.div<
   {
     width?: number;
     bg?: string;
+    isRef?: boolean;
   } & HeightInNumber
 >`
-  width: ${(props) => (props.width ? `${props.width}px` : '0')};
+  width: ${(props) =>
+    props.isRef ? '100%' : props.width ? `${props.width}px` : '0'};
   height: ${(props) => (props.h ? `${props.h}px` : '0')};
   display: block;
   background-color: ${(props) => props.bg ?? 'transparent'};
