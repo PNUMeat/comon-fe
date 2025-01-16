d1d11acd04203180445d06527dc7fc8a31633b89
Jisu Lee <dlwltn0430@naver.com>
2025-01-17 03:52:37 +0900

    design: 플로팅 버튼 폰트 및 디자인 수정

--- src/components/commons/EventFloating/EventFloatingContent.tsx
+++ src/components/commons/EventFloating/EventFloatingContent.tsx
@@ -5,28 +5,30 @@ import { SText } from '../SText';
 
 const Bubble = styled.div`
   position: absolute;
-  bottom: 70px;
-  left: -225px;
+  bottom: 60px;
+  left: -300px;
   background-image: url(${SpeechBubble});
   background-repeat: no-repeat;
   background-size: cover;
-  height: 380px;
+  width: 416px;
+  height: 450px;
 `;
 
 const ContentWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 24px 0px 0px 24px;
-  font-size: 10px;
+  padding: 34px;
+  font-size: 12px;
   line-height: 1.4;
   min-width: 312px;
+  color: #333;
 `;
 
 const ButtonStyle = styled.button`
   position: absolute;
   font-size: 12px;
   right: 32px;
-  bottom: 30px;
+  bottom: 60px;
   width: 100px;
   height: 30px;
   border-radius: 20px;
@@ -65,49 +67,57 @@ export const EventFloatingContent: React.FC<EventFloatingContentProps> = ({
         <Bubble>
           <ContentWrapper>
             <TitleText>
-              <SText color="#ff377f" fontWeight={700} fontFamily={'Pretendard'}>
+              <SText
+                fontSize="11px"
+                color="#ff377f"
+                fontWeight={700}
+                fontFamily={'NanumSquareNeo'}
+              >
                 공지
               </SText>
-              <SText fontSize="16px" fontWeight={700} fontFamily={'Pretendard'}>
+              <SText
+                fontSize="18px"
+                fontWeight={700}
+                fontFamily={'NanumSquareNeo'}
+              >
                 버그 찾기 이벤트
               </SText>
             </TitleText>
             <br />
-            <SText fontFamily={'Pretendard'} fontWeight={300}>
+            <SText fontFamily={'NanumSquareNeo'} fontWeight={500}>
               안녕하세요. 코몬 운영진입니다.
             </SText>
             <br />
-            <SText fontFamily={'Pretendard'} fontWeight={300}>
-              서비스의 사용성을 개선하기 위해 사용자분들이{' '}
-              <UnderlinedText>
-                서비스에서 발견한
-                <br /> 버그를 신고받고 있어요.{' '}
-              </UnderlinedText>
-              중요한 결점부터 사소한 에러까지의 정도에 <br />
-              따라{' '}
+            <SText fontFamily={'NanumSquareNeo'} fontWeight={500}>
+              서비스의 사용성을 개선하기 위해 사용자분들이 서비스에서 발견한
+              <br /> 버그를 신고받고 있어요. 중요한 결점부터 사소한 에러까지의
+              정도
+              <br />에 따라{' '}
               <UnderlinedText>커피 기프티콘을 전송해 드릴 예정</UnderlinedText>
               입니다.
             </SText>
             <br />
-            <SText fontFamily={'Pretendard'} fontWeight={300}>
+            <SText fontFamily={'NanumSquareNeo'} fontWeight={500}>
               대학생들이 만든 프로젝트이기에 미흡한 점이 있음을 널리 이해 부탁
-              <br />
-              드리고, 소중한 의견을 통해 점차 보완해 나갈 것을 약속드립니다.
+              <br /> 드리고, 소중한 의견을 통해 점차 보완해 나갈 것을
+              약속드립니다.
             </SText>
             <br />
-            <SText fontFamily={'Pretendard'} fontWeight={700}>
+            <SText fontFamily={'NanumSquareNeo'} fontWeight={700}>
               참여 조건: 1개 이상의 팀에 참여 & 1개 이상 게시글 작성한 사용자
             </SText>
-            <SText fontFamily={'Pretendard'} fontWeight={700}>
+            <SText fontFamily={'NanumSquareNeo'} fontWeight={700}>
               문의 방법: 하단의 구글 폼 이용
             </SText>
             <br />
-            <SText fontFamily={'Pretendard'} fontWeight={300}>
+            <SText fontFamily={'NanumSquareNeo'} fontWeight={500}>
               감사합니다. 좋은 하루 보내세요!
             </SText>
             <br />
             <Link href="https://forms.gle/6KqTsqQxbvZwAizU9" target="_blank">
-              https://forms.gle/6KqTsqQxbvZwAizU9
+              <SText fontFamily={'NanumSquareNeo'} fontWeight={900}>
+                https://forms.gle/6KqTsqQxbvZwAizU9
+              </SText>
             </Link>
             <br />
             <ButtonStyle onClick={() => setOpen(!open)}>
