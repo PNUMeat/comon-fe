a0ee73a5d5c1c6c2041128091ddda9fd67543080
JaeAnHan <jaean1999@naver.com>
2025-01-17 19:54:28 +0900

    feat: cacher worker 추가

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,27 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import './main.css';
 
+const registerFileCacheWorker = async (): Promise<void> => {
+  if ('serviceWorker' in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register(
+        'src/workers/fileCacher.ts',
+        {
+          type: 'module',
+        }
+      );
+      console.log(
+        'FileCacher ServiceWorker registration successful:',
+        registration
+      );
+    } catch (error) {
+      console.error('FileCacher ServiceWorker registration failed:', error);
+    }
+  }
+};
+
 const queryClient = new QueryClient();
+registerFileCacheWorker();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
