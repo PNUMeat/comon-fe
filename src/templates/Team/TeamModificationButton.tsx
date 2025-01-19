ecc10bc7e16ed3ec28c98ab40cdb052302985429
JaeAnHan <jaean1999@naver.com>
2025-01-19 14:16:15 +0900

    feat: 팀 수정 api 연동

--- src/templates/Team/TeamModificationButton.tsx
+++ src/templates/Team/TeamModificationButton.tsx
@@ -1,15 +1,16 @@
 import { ComonFormSubmitButton } from '@/components/commons/Form/ComonFormSubmitButton';
 
 import { useMemo, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
-// import { useLocation } from 'react-router-dom';
-// import { modifyTeam } from '@/api/team.ts';
+import { modifyTeam } from '@/api/team.ts';
 import click from '@/assets/TeamJoin/click.png';
 import {
   formTextInputAtom,
   formTextareaAtom,
   imageAtom,
-  teamMaxNumAtom, // teamPasswordAtom,
+  teamMaxNumAtom,
+  teamPasswordAtom,
   teamSubjectAtom,
 } from '@/store/form.ts';
 import { serializeForm } from '@/templates/User/utils.ts';
@@ -20,11 +21,11 @@ export const TeamModificationButton = () => {
   const teamExplain = useAtomValue(formTextareaAtom);
   const topic = useAtomValue(teamSubjectAtom);
   const memberLimit = useAtomValue(teamMaxNumAtom);
-  // const password = useAtomValue(teamPasswordAtom);
+  const password = useAtomValue(teamPasswordAtom);
   const image = useAtomValue(imageAtom);
-  // const location = useLocation();
-  // const { teamId } = location.state;
-  // const teamIdInt = parseInt(teamId);
+  const location = useLocation();
+  const { teamId } = location.state;
+  const teamIdInt = parseInt(teamId);
   const [saveCache, setSaveCache] = useState<number>(0);
 
   const cache = useMemo(
@@ -35,27 +36,28 @@ export const TeamModificationButton = () => {
   const onClick = () => {
     console.log('버튼 확인', cache);
     setSaveCache((prev) => prev + 1);
-    // if (
-    //   serializeForm(teamName, teamExplain, image, topic, memberLimit) !== cache
-    // ) {
-    //   modifyTeam({
-    //     teamId: teamIdInt,
-    //     teamName: teamName,
-    //     teamExplain: teamExplain,
-    //     topic: topic,
-    //     image: image,
-    //     memberLimit:
-    //       typeof memberLimit === 'number' ? memberLimit : parseInt(memberLimit),
-    //     // TODO: password 받아올 수 있으면 추가
-    //     password: null,
-    //   })
-    //     .then(() => {
-    //       //     성공했다고 토스트 띄워달라고 합니다
-    //     })
-    //     .catch(() => {
-    //       alert('팀 수정 요청 실패');
-    //     });
-    // }
+    if (
+      serializeForm(teamName, teamExplain, image, topic, memberLimit) !== cache
+    ) {
+      modifyTeam({
+        teamId: teamIdInt,
+        teamName: teamName,
+        teamExplain: teamExplain,
+        topic: topic,
+        image: image,
+        memberLimit:
+          typeof memberLimit === 'number' ? memberLimit : parseInt(memberLimit),
+        // TODO: password 받아올 수 있으면 추가
+        password: password.trim().length === 0 ? null : password,
+      })
+        .then(() => {
+          //     성공했다고 토스트 띄워달라고 합니다
+          alert('팀 수정 성공');
+        })
+        .catch(() => {
+          alert('팀 수정 요청 실패');
+        });
+    }
   };
 
   return (
