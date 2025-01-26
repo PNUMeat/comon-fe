2efddcbe8d36799c88e3d060d246aaa0d2e270bc
JaeAnHan <jaean1999@naver.com>
2025-01-26 17:04:29 +0900

    feat: 로그인 페이지 리다이렉션 복구

--- src/templates/Login/LoginTemplate.tsx
+++ src/templates/Login/LoginTemplate.tsx
@@ -1,38 +1,35 @@
-import { handleCookieOnRedirect } from '@/utils/cookie';
+import { checkIfLoggedIn, handleCookieOnRedirect } from '@/utils/cookie';
 
 import { GradientGlassPanel } from '@/components/commons/GradientGlassPanel';
 import { Spacer } from '@/components/commons/Spacer';
 
 import { Fragment, useEffect } from 'react';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 
-// import { useLocation, useNavigate } from 'react-router-dom';
-// import { PATH } from '@/routes/path';
+import { PATH } from '@/routes/path';
 import { LoginForm } from '@/templates/Login/LoginForm';
 
 export const LoginTemplate = () => {
-  // const navigate = useNavigate();
-  // const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { redirected } = useParams<{ redirected: string }>();
 
   useEffect(() => {
     handleCookieOnRedirect();
-    // if (checkIfLoggedIn()) {
-    //   const sessionRedirect = sessionStorage.getItem('redirect');
-    //   alert(`redirect: ${sessionRedirect}, ${location?.state?.redirect}`);
-    //   if (sessionRedirect) {
-    //     sessionStorage.removeItem('redirect');
-    //   }
-    //   console.error(
-    //     `redirect: ${sessionRedirect}, ${location?.state?.redirect}`
-    //   );
-    //   const redirectDest =
-    //     sessionRedirect ?? location.state?.redirect ?? PATH.HOME;
-    //   // const navigatePath = redirectDest
-    //   //   ? redirectDest === PATH.HOME
-    //   //     ? PATH.TEAMS
-    //   //     : PATH.HOME
-    //   //   : PATH.HOME;
-    //   navigate(redirectDest, { replace: true });
-    // }
+    if (checkIfLoggedIn()) {
+      const sessionRedirect = sessionStorage.getItem('redirect');
+      console.error(
+        `redirect: ${sessionRedirect}, ${location?.state?.redirect}, ??:${redirected}`
+      );
+      const redirectDest =
+        sessionRedirect ?? location.state?.redirect ?? PATH.HOME;
+      // const navigatePath = redirectDest
+      //   ? redirectDest === PATH.HOME
+      //     ? PATH.TEAMS
+      //     : PATH.HOME
+      //   : PATH.HOME;
+      navigate(redirectDest, { replace: true });
+    }
   }, []);
 
   return (
