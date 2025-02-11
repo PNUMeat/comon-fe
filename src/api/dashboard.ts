621b4010a66ee117b7b2d8c30e3cc16ba4840f0d
Jisu Lee <dlwltn0430@naver.com>
2025-02-11 18:28:23 +0900

    chore: 주석 해제

--- src/api/dashboard.ts
+++ src/api/dashboard.ts
@@ -1,6 +1,7 @@
-// import { isDevMode } from '@/utils/cookie.ts';
-// 개발시 주석 해제 필요. 목데이터랑 타입이 안맞음
-// import { subjectMock, teamArticlesMock, teamInfoMock } from '@/api/mocks.ts';
+import { isDevMode } from '@/utils/cookie.ts';
+
+import { subjectMock, teamArticlesMock, teamInfoMock } from '@/api/mocks.ts';
+
 import apiInstance from './apiInstance';
 import { ITeamInfo } from './team';
 import { ServerResponse } from './types';
@@ -57,12 +58,11 @@ export const getTeamInfoAndTags = async (
   year: number,
   month: number
 ): Promise<ITeamInfoAndTagsResponse> => {
-  // 개발시 주석 해제 필요. 목데이터랑 타입이 안맞음
-  // if (isDevMode()) {
-  //   await new Promise((r) => setTimeout(r, 1000));
+  if (isDevMode()) {
+    await new Promise((r) => setTimeout(r, 1000));
 
-  //   return teamInfoMock.data;
-  // }
+    return teamInfoMock.data;
+  }
 
   const res = await apiInstance.get<ServerResponse<ITeamInfoAndTagsResponse>>(
     `/v1/teams/${teamId}/team-page`,
@@ -77,9 +77,9 @@ export const getArticlesByDate = async (
   date: string,
   page: number
 ): Promise<IArticlesByDateResponse> => {
-  // if (isDevMode()) {
-  //   return teamArticlesMock.data;
-  // }
+  if (isDevMode()) {
+    return teamArticlesMock.data;
+  }
 
   const res = await apiInstance.get<ServerResponse<IArticlesByDateResponse>>(
     `/v1/articles/${teamId}/by-date`,
@@ -93,10 +93,9 @@ export const getTeamTopic = async (
   teamId: number,
   date: string
 ): Promise<ITopicResponse> => {
-  // 개발시 주석 해제 필요. 목데이터랑 타입이 안맞음
-  // if (isDevMode()) {
-  //   return subjectMock.data;
-  // }
+  if (isDevMode()) {
+    return subjectMock.data;
+  }
 
   const res = await apiInstance.get<ServerResponse<ITopicResponse>>(
     `/v1/articles/teams/${teamId}/subjects`,
